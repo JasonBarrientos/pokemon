7b5f6ff8c4b36d359be67717d5b713944820dffe
jasonBarrientos <jasonbarrientos.dev@gmail.com>
2025-07-02 05:15:26 -0300

    Se crea paginacion de pokemons

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     forbidNonWhitelisted:true,
-    whitelist: true
+    whitelist: true,
+    transform:true ,
+    transformOptions :{
+      enableImplicitConversion:true
+    }
   }))
   await app.listen(process.env.PORT ?? 3000);
 }
